708cf46c2f730849f519fc871d406f187a1ed9c2
Anastasia Tkacheva <nastre70@gmail.com>
2021-11-29 20:40:02 +1000

    Added Monster, Zombie

---
+++ src/Task24_2_4.java
@@ -0,0 +1,13 @@
+public class Task24_2_4 {
+    public static void main(String[] args) {
+        Zombie zombie = new Zombie("Bob");
+        zombie.attack();
+
+        /*
+        Monster Bob the Zombie was created
+        Monster Bob the Zombie attacked with damage 5
+        Raaaauuughhhh Bob the Zombie growled
+        */
+
+    }
+}
