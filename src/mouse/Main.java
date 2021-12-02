d2dfb9fb19ed784d511d3ea8ab38b3caf2ae34d0
Anastasia <nastre70@gmail.com>
2021-12-02 19:50:33 +1000

    Added MouseListener

---
+++ src/mouse/Main.java
@@ -0,0 +1,16 @@
+package mouse;
+
+import javax.swing.JFrame;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // создаем окно
+        JFrame jf = new JFrame();
+        // устанавливаем ему размеры
+        jf.setSize(400, 300);
+        // и показываем его
+        jf.addMouseListener(new SimpleClickListener());
+        jf.setVisible(true);
+    }
+}
---
+++ src/mouse/SimpleClickListener.java
@@ -0,0 +1,33 @@
+package mouse;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+class SimpleClickListener implements MouseListener{
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Clicked аt " + e.getX() + "," + e.getY());
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+}
